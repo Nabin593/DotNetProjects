6a89e2a16f88714b55e5edad5188871674d8ac4c
Nabin Ghimire <nabin@bentraytech.com>
2024-06-29 21:17:04 +0545

    complete of videoPlaylist-8

--- DatingApp/DatingApp/obj/Debug/net8.0/DatingApp.AssemblyInfo.cs
+++ DatingApp/DatingApp/obj/Debug/net8.0/DatingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c951defe4b67ee38c0de26aff1569c0373d02725")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b417b4b5e815d4430e90c5ee568c5d8454950f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
